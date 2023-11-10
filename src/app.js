0e2ba0a22367b800000f4385978116bbc42d06a9
mmihelca <mmihelca@gmail.com>
2023-11-10 21:52:07 +0100

    updated updateWeather functionality

--- src/app.js
+++ src/app.js
@@ -4,13 +4,21 @@ function updateWeather(response) {
   temperatureElement.innerHTML = temperature;
   city.innerHTML = response.data.city;
 
-  console.log(response.data);
+  let weatherDescription = document.querySelector(".weather-description");
+  weatherDescription.innerHTML = response.data.condition.description;
+
+  let humidity = document.querySelector(".humidity");
+  humidity.innerHTML = `response.data.temperature.humidity`;
+
+  let wind = document.querySelector(".wind");
+  wind.innerHTML = `response.data.wind`;
 }
 
 function searchCity(city) {
   let apiKey = "35ee71bff3b1ft217b0aao934d002bd5";
   let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
   axios.get(apiURL).then(updateWeather);
+  console.log(apiURL);
 }
 
 function handleSearch(event) {
@@ -23,3 +31,23 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearch);
 
 searchCity("gothenburg");
+
+let now = new Date();
+let hour = now.getHours();
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0{minutes}`;
+}
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+let day = days[now.getDay()];
+let dayAndTime = document.querySelector(".day-hour");
+dayAndTime.innerHTML = `${day} ${hour}:${minutes}`;
